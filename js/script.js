1d5955d978662e8e94857d3fa758a60aa6ca91cd
katusepulveda <kat.s.riquelme@gmail.com>
2022-01-12 01:39:44 -0300

    ejercicio js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+let nota1 =(parseFloat(prompt("Ingresa las calificiones que has obtenido en esta asignatura")))
+let nota2 =(parseFloat(prompt("Ingresa las calificiones que has obtenido en esta asignatura")))
+let nota3 =(parseFloat(prompt("Ingresa las calificiones que has obtenido en esta asignatura")))
+
+function pedirNota(nota1,nota2,nota3){
+    console.log("la calificación es: " + notaIngresada);
+}
+
+let resultado = (nota1+nota2+nota3)/3;{
+alert ("Tu promedio final es de " + resultado)
+}
